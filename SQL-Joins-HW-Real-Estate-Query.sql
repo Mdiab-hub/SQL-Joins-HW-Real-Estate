989f6932f91c6fa30cd5cf7b0965037c72700673
Mdiab-hub <Mdiab-hub>
2020-08-30 17:58:21 -0400

    SQL-HW

---
+++ SQL-Joins-HW-Real-Estate-Query.sql
@@ -0,0 +1,18 @@
+/*select price, floor, bedrooms from apartments;
+
+select * FROM tenants where gender = 'Female';
+
+select * FROM apartments where bedrooms > 3;F
+SELECT tenants.name, tenants.age, apartments.bedrooms, apartments.price
+FROM tenants
+INNER JOIN apartments ON tenants.apartment_id = apartments.id; */
+
+
+
+
+SELECT * 
+FROM tenants
+LIMIT 10;
+
+
+
